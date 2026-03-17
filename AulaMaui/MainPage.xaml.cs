fae6b5d33825c7490737b1e56019561b3993f304
Marcos Rocha <zooinm@hotmail.com>
2026-03-16 21:26:00 -0300

    Cadastro de macaco e ajustes no layout da MainPage
    Removido layout antigo da MainPage e adicionado formulário para cadastro de macaco (nome, espécie, doença, imagem). Implementado método novoMacaco para processar o cadastro e exibir confirmação. Criada classe Macaco com propriedades e anotação [Key]. Adicionada imagem feliz.jpg ao projeto.

--- AulaMaui/MainPage.xaml.cs
+++ AulaMaui/MainPage.xaml.cs
@@ -1,10 +1,24 @@
-﻿namespace AulaMaui
+﻿using AulaMaui.Model;
+
+namespace AulaMaui
 {
     public partial class MainPage : ContentPage
     {
+        Macaco macaco = new Macaco();
+       
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void novoMacaco(object sender, EventArgs e)
+        {
+            macaco.Nome = et_nome.Text;
+            macaco.Especie = et_especie.Text;   
+            macaco.Doenca = et_doenca.Text;
+            macaco.Imagem = et_imagem.Text;
+             DisplayAlert("Macaco criado", $"O macaco {macaco.Nome} da espécie {macaco.Especie} com a doença {macaco.Doenca} e imagem {macaco.Imagem} foi criado com sucesso!", "OK","Cancelar");
+
         }
     }
 }
---
+++ AulaMaui/Model/Macaco.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaMaui.Model
+{
+    public class Macaco
+    {
+        public Macaco() { }
+
+        [Key]
+        private int id;
+        private string nome;
+        private string especie;
+        private string doenca;
+        private string imagem;
+        public int Id { get => id; set => id = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Especie { get => especie; set => especie = value; }
+        public string Doenca { get => doenca; set => doenca = value; }
+        public string Imagem { get => imagem; set => imagem = value; }
+    }
+}
