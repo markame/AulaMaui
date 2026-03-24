2c29b6b4e5dba259c24443796d21939becb42831
Marcos Rocha <zooinm@hotmail.com>
2026-03-23 21:40:09 -0300

    Adiciona páginas ListaAnimal e Menu ao projeto MAUI
    Foram criadas as páginas ListaAnimal (com CollectionView para exibir animais) e Menu (com mensagem de boas-vindas), incluindo seus arquivos code-behind. AulaMaui.csproj foi atualizado para compilar os novos XAML.

---
+++ AulaMaui/View/ListaAnimal.xaml.cs
@@ -0,0 +1,9 @@
+namespace AulaMaui.View;
+
+public partial class ListaAnimal : ContentPage
+{
+	public ListaAnimal()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AulaMaui/View/Menu.xaml.cs
@@ -0,0 +1,9 @@
+namespace AulaMaui.View;
+
+public partial class Menu : ContentPage
+{
+	public Menu()
+	{
+		InitializeComponent();
+	}
+}
